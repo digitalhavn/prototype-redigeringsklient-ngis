58206d5f64d45566b6c079a8b63629861745cd98
Erik Salvesen <eriksalv@stud.ntnu.no>
2023-09-11 18:09:16 +0200

    feat: legg til nødvendige request handlers

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,28 +1,44 @@
 import express, { Application } from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { RequestHandler, createProxyMiddleware } from 'http-proxy-middleware';
 import 'dotenv/config';
 
 const app: Application = express();
 
 const NGIS_URL = process.env.NGIS_URL || 'ngis-url';
 const NGIS_TOKEN = process.env.NGIS_TOKEN || 'ngis-token';
-const PORT = 8000;
-const HEADERS = {
+const PORT = 8001;
+const BASE_HEADERS = {
   Authorization: `Basic ${NGIS_TOKEN}`,
   'X-Client-Product-Version': 'NGISProxy 1.0.0',
 };
 
-app.get(
-  '/datasets',
-  createProxyMiddleware({
-    target: `${NGIS_URL}/havn/v1/datasets`,
-    headers: {
-      ...HEADERS,
-      accept: 'application/vnd.kartverket.ngis.dataset+json',
-    },
-    changeOrigin: true,
-  }),
-);
+const proxy: RequestHandler = createProxyMiddleware({
+  target: NGIS_URL,
+  headers: BASE_HEADERS,
+  changeOrigin: true,
+  logLevel: 'debug',
+});
+
+app.get('/datasets', proxy);
+
+app.get('/datasets/:datasetId', proxy);
+
+app.get('/datasets/:datasetId/features', (req, res, next) => {
+  req.headers['accept'] = 'application/vnd.kartverket.sosi+json';
+  proxy(req, res, next);
+});
+
+app.get('/datasets/:datasetId/features/:localId/attributes', (req, res, next) => {
+  req.headers['accept'] = 'application/vnd.kartverket.ngis.attributes+json';
+  proxy(req, res, next);
+});
+
+app.put('/datasets/:datasetId/features/:localId/attributes', (req, res, next) => {
+  req.headers['accept'] = 'application/vnd.kartverket.ngis.attributes+json';
+  proxy(req, res, next);
+});
+
+app.get('/datasets/:datasetId/schema', proxy);
 
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
